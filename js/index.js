9b104de30c39637f358e10df3313cbbdc883243c
WinterSoldier <ayushsingh1315@gmail.com>
2020-08-27 19:50:29 +0530

    init

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
-console.log("Hello World" );
+var name = "";
+name = window.prompt("Oh! visitor from the __future__ what's thy name? :");
+
+
+console.log("Hi! "+name+" Glad you are here\n\n" );
+
 console.log("                  ,d=≥,.,qOp, ");
 console.log("                 ,7'  ''²$    ");
 console.log("                ,7'      '?q$7' ");
@@ -26,12 +31,16 @@ console.log("\n\nAt this point we could really use some help");
 console.log("Join the first DSC in NE India.... DSC-NITA");
 console.log("Positions available: Android, WebDev, ML, GIT, Photoshop");
 console.log("Be a part of the biggest Student-Developers Community and grow with us");
-console.log("Maybe you can even win a fully Google sponsored trip to Goa and San Francisco!!!");
+console.log("Maybe you can even EARN a fully Google sponsored trip to Goa and San Francisco!!!");
 console.log("Moreover you can boast about it on LinkedIn :D");
 console.log("\n");
-console.log("Apply Here: https://bit.ly/3jmE7bp")
+console.log("Apply Here: https://dscnita.github.io/quiz.html")
 
-console.log("\n\n\n\n\n");
+console.log("\n");
 console.log("And yeah... sorry for this terrible invite");
 
-console.log("with <3 from WinterSoldier");
+console.log("with <3 from WinterSoldier13 (github) \n\n");
+
+console.log("Sorry if you are recieving any ERROR messages, honestly I hate 'em too :( ");
+
+
