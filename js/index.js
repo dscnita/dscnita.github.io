26180148af0311bb3891e969399b8e55a612c63a
WinterSoldier13 <ayushsingh1315@gmail.com>
2020-08-27 12:54:56 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,37 @@
+console.log("Hello World" );
+console.log("                  ,d=≥,.,qOp, ");
+console.log("                 ,7'  ''²$    ");
+console.log("                ,7'      '?q$7' ");
+console.log("             ..,$$,.             ");
+console.log("   ,.  .,,--***²''²***--,.  .,  ");
+console.log(" ²   ,p²''              ''²q,   ²");
+console.log(":  ,7'                      '7,  :");
+console.log(" ' $      ,db,      ,db,      $ '");
+console.log("  '$      ²$$²      ²$$²      $'    ");
+console.log("  '$                          $'       "); 
+console.log("   '$.     .,        ,.     .$'");
+console.log("    'b,     '²«»«»«»²'     ,d'");
+console.log("     '²?bn,,          ,,nd?²'");
+console.log("       ,7$ ''²²²²²²²²'' $7,");
+console.log("     ,² ²$              $² ²,");
+console.log("     $  :$              $:  $");
+console.log("     $   $              $   $");
+console.log("     'b  q:            :p  d'");
+console.log("      '²«?$.          .$?»²'");
+console.log("         'b            d'");
+console.log("       ,²²'?,.      .,?'²²,");
+console.log("      ²==--≥²²==--==²²≤--==²");
+
+console.log("\n\nAt this point we could really use some help");
+console.log("Join the first DSC in NE India.... DSC-NITA");
+console.log("Positions available: Android, WebDev, ML, GIT, Photoshop");
+console.log("Be a part of the biggest Student-Developers Community and grow with us");
+console.log("Maybe you can even win a fully Google sponsored trip to Goa and San Francisco!!!");
+console.log("Moreover you can boast about it on LinkedIn :D");
+console.log("\n");
+console.log("Apply Here: https://bit.ly/3jmE7bp")
+
+console.log("\n\n\n\n\n");
+console.log("And yeah... sorry for this terrible invite");
+
+console.log("with <3 from WinterSoldier");
